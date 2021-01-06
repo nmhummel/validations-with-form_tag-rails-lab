a8c73b632dbf10592d9bc75d38ce73e4b2484bd9
nmhummel <natalie.hummel@gmail.com>
2021-01-06 12:45:14 -0600

    commit tree 9a32fd7edac6de382419e9b84e8831486717d712

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} must be Fiction or Non-Fiction" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/authors/new", to: "authors#new", as: "new_author"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  post "/authors", to: "authors#create"
+  patch "/authors/:id", to: "authors#update"
+
+  get "/posts/new", to: "posts#new", as: "new_post"
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  post "/posts", to: "posts#create"
+  patch "/posts/:id", to: "posts#update"
 end
